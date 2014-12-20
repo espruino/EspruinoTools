bc3d903ec23875731dc67f568ac22aa27c04b3be
Gordon Williams (u36) <gw@pur3.co.uk>
2014-12-20 16:01:32 +0000

    arguments, and if statement

--- plugins/compiler.js
+++ plugins/compiler.js
@@ -44,6 +44,24 @@
           if (v) x.call("jsvUnLock",v);
         });
       },
+      "IfStatement" : function(x, node) {
+        var v = x.handle(node.test);
+        var vbool = x.call("jsvGetBool", v);
+        x.out("  pop {r0}");
+        x.out("  cmp r0, #0");
+        var lFalse = x.getNewLabel("_if_false");
+        x.out("  bne "+lFalse);
+        x.handle(node.consequent);        
+        if (node.alternate) {
+          var lEnd =  x.getNewLabel("_if_end");
+          x.out("  b "+lEnd);
+          x.out(lFalse+":");
+          x.handle(node.alternale);
+          x.out(lEnd+":");
+        } else {
+          x.out(lFalse+":");
+        }        
+      },
       "ReturnStatement" : function(x, node) {
         var v = x.handle(node.argument);
         if (v) x.out("  pop {r0}");
@@ -67,17 +85,19 @@
       "Literal" : function(x, node) {
         if (typeof node.value == "string")
           return x.call("jsvNewFromString", x.addBinaryData(node.value));
+        else if (typeof node.value == "boolean")
+          return x.call("jsvNewFromBool", x.addBinaryData(node.value)); // TODO: store number inline
         else if (typeof node.value == "number") {
           if (isFloat(node.value)) 
-            return x.call("jsvNewFromFloat", x.addBinaryData(node.value));
+            return x.call("jsvNewFromFloat", x.addBinaryData(node.value)); // TODO: store number inline
           else 
-            return x.call("jsvNewFromInteger", x.addBinaryData(node.value));
-        } else console.warn("Unknown literal type "+typeof node.value);
+            return x.call("jsvNewFromInteger", x.addBinaryData(node.value)); // TODO: store number inline
+        } else throw new Error("Unknown literal type "+typeof node.value);
       },        
       "Identifier" : function(x, node) {
         var localOffset = x.getLocalOffset(node.name);
         if (localOffset !== undefined) {
-          x.out("  ldr r0, [sp,#"+localOffset+"]", "FIXME - need offset");
+          x.out("  ldr r0, [r7, #"+(localOffset*4)+"]", "Get argument "+node.name);
           x.out("  push {r0}");
           return stackValue();
         } else { 
@@ -102,6 +122,7 @@
     var constData = []; // constants that get shoved at the end of the code
     var trampolines = []; // which trampoline functions we need to make
     var assembly = []; // assembly that is output
+    var labelCounter = 0;
     var x = {    
       "getLocalOffset" : function (name) { // get the offset of the local, or undefined
         if (name in locals) return locals[name].offset;
@@ -110,9 +131,13 @@
       "handle": function(node) {
         if (node.type in handlers)
           return handlers[node.type](x, node);
-        console.warn("No handler for "+node.type);
-        console.warn(node);
+        console.warn("Unknown", node);
+        throw new Error("No handler for "+node.type);
+
         return undefined;
+      },
+      "getNewLabel" : function(helper) {
+         return "label_"+labelCounter+helper;
       },
       // Store binary data and return a pointer
       "addBinaryData": function(data) {
@@ -122,7 +147,7 @@
         if (typeof data == "string") {
           data += "\0";
           prefix = "*"; // it's a pointer
-        } else if (typeof data == "number") {
+        } else if (typeof data == "number" || typeof data == "boolean") {
           if (isFloat(data)) {
             var b = new ArrayBuffer(8);
             new Float64Array(b)[0] = data;
@@ -132,7 +157,9 @@
             new Int32Array(b)[0] = data;
             data = String.fromCharCode.apply(null,new Uint8Array(b));
           }
-        } else console.warn("Unknown data type "+typeof node.value);
+        } else {
+          throw new Error("Unknown data type "+typeof data );
+        }
         
         // check for dups, work out offset
         for (var n in constData) {
@@ -187,12 +214,19 @@
           return undefined;
       }
     };
-    x.out("  push {r7,lr}", "Save registers that might get overwritten");
+    // r6 is for trampolining
+    // r7 is a frame pointer
+    x.out("  push {r6,r7,lr}", "Save registers that might get overwritten");  
+    var stackItems = node.params.length;
+    if (stackItems>4) stackItems = 4; // only first 4 are in registers
+    x.out("  sub sp, #"+(stackItems*4), "add space for local vars of the stack"); 
+    x.out("  add r7, sp, #0", "copy current stack pointer to r7"); 
     // Parse parameters and push onto stack    
     node.params.forEach(function( paramNode, idx) { 
-      locals[paramNode.name] = { type : "param", offset : idx };
-      x.out("  push {r"+idx+"}", "push params onto stack - TODO: could do this in bulk");
-      // TODO: add code to clear up at exit
+      // FIXME: >4 arguments seems to shift them all by 1 somehow. Maybe the first one went on the stack and others in registers?
+      locals[paramNode.name] = { type : "param", offset : (idx<4) ? idx : idx+3/*because we pushed 3 items above*/ };
+      if (idx<4) // only first 4 on stack
+        x.out("  str r"+idx+", [r7, #"+(idx*4)+"]", "copy params onto stack");
       params.push("JsVar"); 
     }); 
     // Serialise all statements
@@ -202,18 +236,16 @@
       if (v) x.call("jsvUnLock",v);
     });  
     x.out("end_of_fn:");  
-    node.params.forEach(function( paramNode, idx) { 
-      x.out("  pop {r3}", "pop params off the stack - TODO: just add/sub stack ptr"); 
-    }); 
-    x.out("  pop {r7,lr}", "Restore registers that might get overwritten");
+    x.out("  add sp, #"+(stackItems*4), "take loal vars of the stack"); 
+    x.out("  pop {r6,r7,lr}", "Restore registers that might get overwritten");
     x.out("  bx lr"); 
     // add trampoline functions
     trampolines.forEach(function(tramp) {
       if (x.getCodeSize()&2) x.out("  nop"); // need to align this for the ldr instruction
       x.out(tramp+":");
-      x.out("  ldr    r7, exports");
-      x.out("  ldr    r7, [r7, #"+(exportNames.indexOf(tramp)*4)+"]", "Get fn address");
-      x.out("  bx r7");
+      x.out("  ldr    r6, exports");
+      x.out("  ldr    r6, [r6, #"+(exportNames.indexOf(tramp)*4)+"]", "Get fn address");
+      x.out("  bx r6");
     });
     // work out length and align to a word boundary
     if (x.getCodeSize()&2) x.out("  nop");
@@ -250,7 +282,11 @@
             node.body.body[0].type=="ExpressionStatement" &&
             node.body.body[0].expression.type=="Literal" && 
             node.body.body[0].expression.value=="compiled") {
-          var asm = compileFunction(node);
+          try {
+            var asm = compileFunction(node);
+          } catch (err) {
+            console.warn(err.toString());
+          }
           if (asm) {
             asm = asm;
             //console.log(asm);
