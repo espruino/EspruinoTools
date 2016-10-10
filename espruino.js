2341454a236af1c0aa5d5e25f0e4b6be8a116d4f
Gordon Williams <gw@pur3.co.uk>
2016-10-10 13:17:38 +0100

    add some code to try and detect broken modules

--- espruino.js
+++ espruino.js
@@ -4,7 +4,7 @@
  This Source Code is subject to the terms of the Mozilla Public
  License, v2.0. If a copy of the MPL was not distributed with this
  file, You can obtain one at http://mozilla.org/MPL/2.0/.
- 
+
  ------------------------------------------------------------------
   Initialisation code
  ------------------------------------------------------------------
@@ -17,14 +17,14 @@ var Espruino;
 
   /** List of processors. These are functions that are called one
    * after the other with the data received from the last one.
-   * 
+   *
    * Common processors are:
    *
-   *   sending              - sending code to Espruino (no data) 
+   *   sending              - sending code to Espruino (no data)
    *   transformForEspruino - transform code ready to be sent to Espruino
    *   transformModuleForEspruino - transform module code before it's sent to Espruino with Modules.addCached (we only do this if we don't think it's been minified before)
-   *   connected            - connected to Espruino (no data) 
-   *   disconnected         - disconnected from Espruino (no data) 
+   *   connected            - connected to Espruino (no data)
+   *   disconnected         - disconnected from Espruino (no data)
    *   environmentVar       - Board's process.env loaded (object to be saved into Espruino.Env.environmentData)
    *   boardJSONLoaded      - Board's JSON was loaded into environmentVar
    *   getModule            - Called with data={moduleName:"foo", moduleCode:undefined} - moduleCode should be filled in if the module can be found
@@ -34,23 +34,23 @@ var Espruino;
    *   terminalNewLine      - When we get a new line on the terminal, this gets called with the last line's contents
    *   debugMode            - called with true or false when debug mode is entered or left
    *   editorHover          - called with { node : htmlNode, showTooltip : function(htmlNode) } when something is hovered over
-   **/  
+   **/
   var processors = {};
-  
-  function init() {    
-    
+
+  function init() {
+
     Espruino.Core.Config.loadConfiguration(function() {
-      // Initialise all modules    
-      function initModule(modName, mod) {      
+      // Initialise all modules
+      function initModule(modName, mod) {
         console.log("Initialising "+modName);
         if (mod.init !== undefined)
           mod.init();
       }
-      
+
       var module;
       for (module in Espruino.Core) initModule(module, Espruino.Core[module]);
       for (module in Espruino.Plugins) initModule(module, Espruino.Plugins[module]);
-      
+
       callProcessor("initialised", undefined, function() {
         // We need the delay because of background.js's url_handler...
         setTimeout(function() {
@@ -59,7 +59,7 @@ var Espruino;
       });
     });
   }
-  
+
   // workaround for broken chrome on Mac
   if (navigator.userAgent.indexOf("Mac OS X")>=0 &&
       navigator.userAgent.indexOf("Chrome/33.0.1750")>=0) {
@@ -67,14 +67,14 @@ var Espruino;
   } else {
     $(document).ready(init);
   }
-  
+
   /** Add a processor function of type function(data,callback) */
   function addProcessor(eventType, processor) {
     if (processors[eventType]===undefined)
       processors[eventType] = [];
     processors[eventType].push(processor);
   }
-  
+
   /** Call a processor function */
   function callProcessor(eventType, data, callback) {
     var p = processors[eventType];
@@ -85,19 +85,23 @@ var Espruino;
     }
     // now go through all processors
     var n = 0;
+    var cbCalled = false;
     var cb = function(inData) {
+      if (cbCalled) throw new Error("Internal error in "+eventType+" processor. Callback is called TWICE.");
+      cbCalled = true;
       if (n < p.length) {
+        cbCalled = false;
         p[n++](inData, cb);
       } else {
         if (callback!==undefined) callback(inData);
-      }        
+      }
     };
     cb(data);
-  }  
-  
+  }
+
   // -----------------------------------
-  Espruino = { 
-    Core : { }, 
+  Espruino = {
+    Core : { },
     Plugins : { },
     addProcessor : addProcessor,
     callProcessor : callProcessor,
@@ -107,5 +111,3 @@ var Espruino;
 
   return Espruino;
 })();
-
-
