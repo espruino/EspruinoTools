b3fcc64a8a57f5dbaf2a77989bea6b7d740a5dd8
Gordon Williams <gw@pur3.co.uk>
2017-03-08 09:34:41 +0000

    use https for modules

--- core/modules.js
+++ core/modules.js
@@ -18,7 +18,7 @@
       name : "Module URL",
       description : "Where to search online for modules when `require()` is used",
       type : "string",
-      defaultValue : (Espruino.Core.Utils.needsHTTPS()?"https":"http")+"://www.espruino.com/modules"
+      defaultValue : "https://www.espruino.com/modules"
     });
     Espruino.Core.Config.add("MODULE_EXTENSIONS", {
       section : "Communications",
