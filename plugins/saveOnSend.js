5417c37e939478871efb339c8d086bf96bf28c34
Gordon Williams <gw@pur3.co.uk>
2020-02-05 14:22:02 +0000

    Add ability to save to a file in Storage (not just 'BootCode')

--- plugins/saveOnSend.js
+++ plugins/saveOnSend.js
@@ -25,8 +25,16 @@
         0: "To RAM (default) - execute code while uploading. Use 'save()' to save a RAM image to Flash",
         1: "Direct to Flash (execute code at boot)",
         2: "Direct to Flash (execute code at boot, even after 'reset()') - USE WITH CARE",
+        3: "To Storage File (see 'File in Storage to send to')",
       },
       defaultValue : 0
+    });
+    Espruino.Core.Config.add("SAVE_STORAGE_FILE", {
+      section : "Communications",
+      name : "File in Storage to send to",
+      descriptionHTML : "If <code>Save on Send</code> is set to <code>To Storage File</code>, this is the name of the file to write to.",
+      type : "string",
+      defaultValue : ""
     });
     Espruino.addProcessor("transformForEspruino", function(code, callback) {
       wrap(code, callback);
@@ -35,14 +43,15 @@
 
   function wrap(code, callback) {
     var isFlashPersistent = Espruino.Config.SAVE_ON_SEND == 2;
-    var isFlashUpload = Espruino.Config.SAVE_ON_SEND == 1 || isFlashPersistent;
+    var isStorageUpload = Espruino.Config.SAVE_ON_SEND == 3;
+    var isFlashUpload = Espruino.Config.SAVE_ON_SEND == 1 || isFlashPersistent || isStorageUpload;
     if (!isFlashUpload) return callback(code);
 
     // Check environment vars
     var hasStorage = false;
     var ENV = Espruino.Core.Env.getData();
-    if (ENV && 
-        ENV.VERSION_MAJOR && 
+    if (ENV &&
+        ENV.VERSION_MAJOR &&
         ENV.VERSION_MINOR!==undefined) {
       if (ENV.VERSION_MAJOR>1 ||
           ENV.VERSION_MINOR>=96) {
@@ -53,17 +62,35 @@
     //
     console.log("Uploading "+code.length+" bytes to flash");
     if (!hasStorage) { // old style
-      Espruino.Core.Notifications.error("You have pre-1v96 firmware. Upload size is limited by available RAM");
-      code = "E.setBootCode("+JSON.stringify(code)+(isFlashPersistent?",true":"")+");load()\n";
+      if (isStorageUpload) {
+        Espruino.Core.Notifications.error("You have pre-1v96 firmware - unable to upload to Storage");
+        code = "";
+      } else {
+        Espruino.Core.Notifications.error("You have pre-1v96 firmware. Upload size is limited by available RAM");
+        code = "E.setBootCode("+JSON.stringify(code)+(isFlashPersistent?",true":"")+");load()\n";
+      }
     } else { // new style
-      var filename = isFlashPersistent ? ".bootrst" : ".bootcde";
-      var CHUNKSIZE = 1024;
-      var newCode = [];
-      var len = code.length;
-      newCode.push('require("Storage").write("'+filename+'",'+JSON.stringify(code.substr(0,CHUNKSIZE))+',0,'+len+');');
-      for (var i=CHUNKSIZE;i<len;i+=CHUNKSIZE)
-        newCode.push('require("Storage").write("'+filename+'",'+JSON.stringify(code.substr(i,CHUNKSIZE))+','+i+');');
-      code = newCode.join("\n")+"\nload()\n";
+      var filename;
+      if (isStorageUpload)
+        filename = Espruino.Config.SAVE_STORAGE_FILE;
+      else
+        filename = isFlashPersistent ? ".bootrst" : ".bootcde";
+      if (!filename || filename.length>8) {
+        Espruino.Core.Notifications.error("Invalid Storage file name "+JSON.stringify(filename));
+        code = "";
+      } else {
+        var CHUNKSIZE = 1024;
+        var newCode = [];
+        var len = code.length;
+        newCode.push('require("Storage").write("'+filename+'",'+JSON.stringify(code.substr(0,CHUNKSIZE))+',0,'+len+');');
+        for (var i=CHUNKSIZE;i<len;i+=CHUNKSIZE)
+          newCode.push('require("Storage").write("'+filename+'",'+JSON.stringify(code.substr(i,CHUNKSIZE))+','+i+');');
+        code = newCode.join("\n");
+        if (isStorageUpload)
+          code += "\nload("+JSON.stringify(filename)+")\n";
+        else
+          code += "\nload()\n";
+      }
     }
     callback(code);
   }
