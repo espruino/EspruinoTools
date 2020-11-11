fbc4a6c7e83e68ce07cbddbb27bdaff60b3a50ab
Gordon Williams <gw@pur3.co.uk>
2020-11-11 08:28:51 +0000

    update web serial for Chrome 86

--- core/serial_web_serial.js
+++ core/serial_web_serial.js
@@ -93,7 +93,8 @@
     promise.then(function(port) {
       Espruino.Core.Status.setStatus("Connecting to serial port");
       serialPort = port;
-      return port.open({ baudrate: parseInt(Espruino.Config.BAUD_RATE) });
+      var br = parseInt(Espruino.Config.BAUD_RATE);
+      return port.open({ baudrate: br/*old*/, baudRate: br/*new*/ });
     }).then(function () {
       function readLoop() {
         serialPortReader = serialPort.readable.getReader();
