58e96cef5e3ed119897eb2e6e3700795428b1a29
Gordon Williams <gw@pur3.co.uk>
2023-04-28 16:36:53 +0100

    allowed bridge->ide->bridge shuffle to allow the ide to connect to the bridge when it's the IDE that shows the QR code

--- core/serial_webrtc.js
+++ core/serial_webrtc.js
@@ -26,125 +26,102 @@ var webrtc; // Our WebRTC connection
 
   var serialReceiveCallback;
   var serialDisconnectCallback;
+  var popup; // popup window from showPairingPopup that goes away when the Bridge connects
 
   function print(txt) {
     Espruino.Core.Terminal.outputDataHandler(txt);
   }
 
-  var getPorts=function(callback) {
-    if (webrtc) {
-      webrtc.getPorts(callback);
-    } else {
-      callback([{path:'Remote Connection', description:'Connect with a phone', type : "bluetooth"}], true/*instantPorts*/);
+  webrtc = webrtcInit({
+    bridge:false, 
+    onStatus : function(s) {
+      print(s);
+    },
+    onPeerID : function(s) {
+      
+      //
+    },
+    onVideoStream : function(stream) {
+      if (stream) Espruino.Config.set("SHOW_WEBCAM_ICON", 1); // force webcam icon
+      Espruino.Plugins.Webcam.displayMediaStream(stream);
+    },
+    onConnected : function() {
+      // peer connected, remove the popupa and show the port selector
+      if (popup) {
+        popup.close(); // popup.onClose will call openCallback(undefined);
+        popup = undefined;
+      } 
+      setTimeout(() => {
+        Espruino.Core.MenuPortSelector.showPortSelector()
+      }, 100); // now open the port selector again and we should hopefully see some stuff!
+    },
+    onPortReceived : function(data) {
+      serialReceiveCallback(Espruino.Core.Utils.stringToArrayBuffer(data));
+    },
+    onPortDisconnected : function() {
+      serialDisconnectCallback();
     }
+    });  
+
+  var getPorts=function(callback) {
+    if (webrtc.connections.length) {
+      // If we have a connection, great - use it to get ports
+      webrtc.getPorts(callback);
+    } else if (webrtc.peerId) {
+      // If no connection, pop up an option to enable it which we will handle in openSerial
+      callback([{path:'Remote Connection', description:'Connect via another device', type : "bluetooth"}], true/*instantPorts*/);
+    } else
+      callback([]); // peer connection failed - ignore this
   };
 
+  function showPairingPopup() {
+    var url = window.location.href+"remote?id="+webrtc.peerId;
+    var qrDiv = document.getElementById("serial_peer_qrcode");
+    if (qrDiv==null) {
+      qrDiv = document.createElement("div");
+      qrDiv.id = "serial_peer_qrcode";
+      qrDiv.style = "display:none";
+      document.body.append(qrDiv);
+    }
+    qrDiv.innerHTML = "";
+    var qrcode = new QRCode(qrDiv, {
+      text: url,
+      colorDark : "#ffffff",
+      colorLight : "#000000",
+      correctLevel : QRCode.CorrectLevel.H
+    });
+    setTimeout(function() {
+      // qrcode doesn't complete immediately and there is no callback
+      popup = Espruino.Core.App.openPopup({
+        title: "Remote Connection",
+        contents: 
+`<div style="padding:20px;text-align:center;">
+Please scan the QR code below with your phone or copy/paste the URL to start a connection<br/>
+<div style="padding:20px">${qrDiv.innerHTML}<br/></div><a href="${url}" target="_blank">${url}</a>
+</div>`,
+        position: "center",
+        onClose: function() {
+          popup = undefined;
+        }
+      });   
+    }, 200);
+  }
+
   var openSerial=function(serialPort, openCallback, receiveCallback, disconnectCallback) {
-    if (webrtc) {
-      webrtc.portConnect(serialPort, function() {
-        serialReceiveCallback = receiveCallback;
-        serialDisconnectCallback = disconnectCallback;
-        openCallback({});
-      });
+    if (!webrtc.connections.length) { // we're not connected
+      openCallback(undefined); // cancel the connection
+      // Create a popup with the QR code
+      if (webrtc.peerId)
+        showPairingPopup();
       return;
     }
-    var popup;
-    webrtc = webrtcInit({
-      bridge:false, 
-      onStatus : function(s) {
-        print(s);
-      },
-      onPeerID : function(s) {
-        var url = window.location.href+"remote?id="+peer.id;
-        var qrDiv = document.getElementById("serial_peer_qrcode");
-        if (qrDiv==null) {
-          qrDiv = document.createElement("div");
-          qrDiv.id = "serial_peer_qrcode";
-          qrDiv.style = "display:none";
-          document.body.append(qrDiv);
-        }
-        qrDiv.innerHTML = "";
-        var qrcode = new QRCode(qrDiv, {
-          text: url,
-          colorDark : "#ffffff",
-          colorLight : "#000000",
-          correctLevel : QRCode.CorrectLevel.H
-        });
-        setTimeout(function() {
-          // qrcode doesn't complete immediately and there is no callback
-          popup = Espruino.Core.App.openPopup({
-            title: "Remote Connection",
-            contents: 
-`<div style="padding:20px;text-align:center;">
-  Please scan the QR code below with your phone or copy/paste the URL to start a connection<br/>
-  <div style="padding:20px">${qrDiv.innerHTML}<br/></div><a href="${url}" target="_blank">${url}</a>
-</div>`,
-            position: "center",
-            onClose: function() {
-              // cancelled!
-              if (popup) openCallback(undefined); // signal failure
-              popup = undefined;
-            }
-          });   
-        }, 200);
-        //
-      },
-      onVideoStream : function(stream) {
-        Espruino.Config.set("SHOW_WEBCAM_ICON", 1); // force webcam icon
-        Espruino.Plugins.Webcam.displayMediaStream(stream);
-      },
-      onConnected : function() {
-        if (popup) {
-          popup.close(); // popup.onClose will call openCallback(undefined);
-          popup = undefined;
-        } 
-        setTimeout(() => {
-          Espruino.Core.MenuPortSelector.showPortSelector()
-        }, 100); // now open the port selector again and we should hopefully see some stuff!
-      },
-      onPortReceived : function(data) {
-        serialReceiveCallback(data);
-      },
-      onPortDisconnected : function() {
-        serialDisconnectCallback();
-      }
-      });  
-    };
-
-
-    /*
-
-
-    dataWrittenCallbacks = [];
-    ws.onerror = function(event) {
-      connectCallback(undefined);
-      ws = undefined;
-    };
-    ws.onopen = function() {
-      if (Espruino.Config.RELAY_KEY) {
-        ws.send("\x10"+Espruino.Config.RELAY_KEY);
-      }
-      openCallback("Hello");
-      ws.onerror = undefined;
-      ws.onmessage = function (event) {
-        //console.log("MSG:"+event.data);
-        if (event.data[0]=="\x00") {
-          receiveCallback(Espruino.Core.Utils.stringToArrayBuffer(event.data.substr(1)));
-        } else if (event.data[0]=="\x02") {
-          // if it's a data written callback, execute it
-          var c = dataWrittenCallbacks.shift();
-          if (c) c();
-        }
-      };
-      ws.onclose = function(event) {
-        currentDevice = undefined;
-        if (disconnectCallback) disconnectCallback();
-        disconnectCallback = undefined;
-        ws = undefined;
-      };
-    }*/
-  
-
+    // All ok - forward our connection!
+    webrtc.portConnect(serialPort, function() {
+      serialReceiveCallback = receiveCallback;
+      serialDisconnectCallback = disconnectCallback;
+      openCallback({});
+    });
+  };
 
   // ----------------------------------------------------------
   Espruino.Core.Serial.devices.push({
--- libs/webrtc-connection.js
+++ libs/webrtc-connection.js
@@ -6,9 +6,14 @@ This needs:
 EspruinoWebIDE/js/libs/peerjs.min.js
 
 * The 'Bridge' connects direct to Bluetooth/Bangle.js - it is the Client
-* The Web IDE is a server
+* The 'Client' (Web IDE) is a server
 
 Protocol:
+
+* Both Bridge and Client have servers
+* If the 'Bridge' connects to the 'Client', the Client is immediately sent a {t:"serverId",id:"..."} packet
+  * The Client then closes and connects to the Bridge
+* When the Client connects to the Bridge, things are working correctly
 
 Packets Sent:
 
@@ -27,16 +32,13 @@ Packets Sent:
 
 TODO:
 
-* Add icon for Webcam display - when enabled it should allow forwarding over the peer connection
-* The IDE is currently the server because it needs to send an ID to the bridge via QR Code/Link 
-  - this isn't great especially for CLI apps, and really the bridge should be the server
-  - maybe we actually allow the bridge to connect to the IDE and then tell it where to connect to??
+* The Client (IDE) side needs to be able to remember the Bridge's ID and autoconnect
+* We need to have repeated retries for the connection
+* Clicking top-left in the bridge to pair should automatically disconnect
 
 */
 
 var peer;
-var conn;
-var lastPeerId = null;
 var callbacks = {};
 
 /* Create a connection:
@@ -44,7 +46,7 @@ options = {
   bridge : bool 
      //  true = we're the one communicating with the Bangle
      //  false = we're communicating with the user
-  connectToPeerID : string // if set, we connect to this peer as soon as we start
+  //connectToPeerID : string // if set, we connect to this peer as soon as we start
   onStatus : function(msg) // show status message   
   onPeerID : function(id) // we have a peer ID now
   onConnected : function // a peer has connected
@@ -61,63 +63,71 @@ options = {
 Return options with extras added:
 
 {
-  send : function(d) // send characters
+  connect : function(id) // connect to the given peer ID
+  connectSendPeerId  : function(id) // BRIDGE: Connect to the client in order to tell it to connect back to us!
   getPorts : function(cb) // get current ports
   portConnect : function(port, cb) // CLIENT: Connect to a device
   portDisconnect : function(cb)    // CLIENT: Disconnect
   onPortDisconnected : function()  // BRIDGE: Signal client was disconnected
   portWrite : function(data, cb)   // CLIENT: Send data
   onPortReceived : function(data)    // BRIDGE: Got data from device
+
+  peerId : "..."  // The peer ID of *this* peer
+  connections : [] // list of open connections
+  connection : undefined // the current open connection (since we are only supporting one connection right now)
 }
 */
 function webrtcInit(options) {
   options = options||{};
 
+  options.peerId = null;
+  options.connections = [];
+  options.connection = undefined; // active connection
+
   if (!options.bridge &&
       "undefined"!=typeof window &&
       window.localStorage &&
       window.localStorage.getItem("WEBRTC_PEER_ID"))
-    lastPeerId = window.localStorage.getItem("WEBRTC_PEER_ID");
+    options.peerId = window.localStorage.getItem("WEBRTC_PEER_ID");
 
-  peer = new Peer(lastPeerId, {
+  peer = new Peer(options.peerId, {
     debug: 2
   });
   peer.on('open', function (id) {
-      // Workaround for peer.reconnect deleting previous id
-      if (peer.id === lastPeerId) {
-          console.log('Received null id from peer open');
-          peer.id = lastPeerId;
-      } else {
-          lastPeerId = peer.id;
-          if (!options.bridge &&
-            "undefined"!=typeof window &&
-            window.localStorage &&
-            window.localStorage.setItem("WEBRTC_PEER_ID", peer.id));
-      }
+    // Workaround for peer.reconnect deleting previous id
+    if (peer.id === null) {
+        console.log('[WebRTC] Received null id from peer open');
+        peer.id = options.peerId;
+    } else {
+      options.peerId = peer.id;
+        if (!options.bridge &&
+          "undefined"!=typeof window &&
+          window.localStorage &&
+          window.localStorage.setItem("WEBRTC_PEER_ID", peer.id));
+    }
+    options.peerId = peer.id;
 
-      console.log('[WebRTC] Peer ID: ' + peer.id);
-      if (options.onPeerID)
-        options.onPeerID(peer.id);
-
-      webrtcConnect(options);
+    console.log('[WebRTC] Peer ID: ' + peer.id);
+    if (options.onPeerID)
+      options.onPeerID(peer.id);
   });
   peer.on('disconnected', function () {
-      options.onStatus("Connection lost. Reconnecting...");
-      console.log('[WebRTC] Connection lost. Reconnecting....');
+    options.onStatus("Connection lost. Reconnecting...");
+    console.log('[WebRTC] Connection lost. Reconnecting....');
 
-      // Workaround for peer.reconnect deleting previous id
-      peer.id = lastPeerId;
-      peer._lastServerId = lastPeerId;
-      peer.reconnect();
+    // Workaround for peer.reconnect deleting previous id
+    peer.id = options.peerId;
+    peer._lastServerId = options.peerId;
+    peer.reconnect();
   });
   peer.on('close', function() {
-      conn = null;
-      options.onStatus("Connection destroyed. Please refresh");
-      console.log('Connection destroyed');
+    conn = null;
+    options.onStatus("Connection destroyed. Please refresh");
+    console.log('Connection destroyed');
   });
   peer.on('error', function (err) {
-      console.log(err);
-      options.onStatus("ERROR: "+err);
+    console.log(err);
+    options.onStatus("ERROR: "+err);
   });
   peer.on('call', function(call) {    
     if (options.onVideoStream) {
@@ -125,184 +135,223 @@ function webrtcInit(options) {
         // `stream` is the MediaStream of the remote peer.
         options.onVideoStream(stream);
       });
+      call.on('close', function() {
+        // this seems broken https://github.com/peers/peerjs/issues/636
+        options.onVideoStream(undefined);
+      });
       call.answer(/*mediaStream*/); // answer - not sending a stream back...
     }
   });
 
-  peer.on('connection', function (c) {
+  peer.on('connection', function (conn) {
     console.log("[WebRTC] Incoming connection");
-    // Allow only a single connection
-    if (conn && conn.open) {
-        c.on('open', function() {
-          console.log("[WebRTC] Already connected to another client - disconnecting");
-          c.send({t:"err",msg:"Already connected to another client"});
-          setTimeout(function() { c.close(); }, 500);
-        });
-        return;
+    /* If we're a client, we just want to listen for connections
+    which will tell us where *we* should connect */
+    if (!options.bridge) {
+      console.log("[WebRTC] ServerId: We're a client - wait for a serverId packet");
+      conn.on('open', function () {
+        console.log("[WebRTC] ServerId: Connected to: " + conn.peer);
+      });
+      conn.on('data', function (data) {        
+        if ("object" == typeof data &&
+            data.t=="serverId" &&
+            data.id) {
+          console.log("[WebRTC] ServerId: Got server ID : "+data.id);       
+          options.connect(data.id);
+        } else 
+          console.log("[WebRTC] ServerId: Unknown packet!",data);   
+      });
+      conn.on('close', function () {
+        console.log("[WebRTC] ServerId: Connection closed");
+      });  
+      return;
     }
-
-    conn = c;
+    // We're a bridge - handle this connection!
+    // Allow only a single connection
+    if (options.connection && options.connection.open) {
+      conn.on('open', function() {
+        console.log("[WebRTC] Already connected to another client - disconnecting");
+        conn.send({t:"err",msg:"Already connected to another client"});
+        setTimeout(function() { conn.close(); }, 500);
+      });
+      return;
+    }
     console.log("[WebRTC] Connected to: " + conn.peer);
     options.onStatus("Connected to: " + conn.peer);
     if (options.onConnected) options.onConnected();
-    webrtcAddHandlers(conn, options);
+    webrtcAddHandlers(conn);
   });
-/*
+
   Object.assign(options, { // ============== COMMON
-    send : function(d) {
-      if (conn) conn.send({t:"send", data:d});
-    },
-  });*/
+    connect : function(id) {
+      console.log("[webRTC] Connecting...");
+      var conn = peer.connect(id, {
+          reliable: true
+      });
+      webrtcAddHandlers(conn);
+    }
+  });
 
   if (options.bridge) {
     Object.assign(options, { // ============== BRIDGE
       getPorts : function(cb) { // CLIENT
         callbackAddWithTimeout("getPorts", cb, 2000);
-        conn.send({t:"getPorts"});      
+        options.connection.send({t:"getPorts"});      
       },
       portConnect : function(port, cb) { // CLIENT
         callbackAddWithTimeout("connect", cb, 2000);
-        conn.send({t:"connect", port:port});      
+        options.connection.send({t:"connect", port:port});      
       },
       portDisconnect : function(cb) { // CLIENT
         callbackAddWithTimeout("disconnect", cb, 2000);
-        conn.send({t:"disconnect"});      
+        options.connection.send({t:"disconnect"});      
       },
       onPortDisconnected : function() { 
-        conn.send({t:"disconnected"});   
+        options.connection.send({t:"disconnected"});   
       },
       onPortReceived : function(data) { 
-        conn.send({t:"received", data});   
+        options.connection.send({t:"received", data});   
       },
       portWrite : function(data, cb) { // CLIENT
         callbackAddWithTimeout("write", cb, 2000);
-        conn.send({t:"write", data:data});      
+        options.connection.send({t:"write", data:data});      
       },
       connectVideo : function (stream) {
-        if (!options.connectToPeerID) {
-          console.log("webrtc.connectVideo no peer id");
+        if (!webrtc.connections.length) {
+          console.log("webrtc.connectVideo no active connections");
           return;
         }
-        peer.call(options.connectToPeerID, stream);
+        webrtc.connections.forEach(c => {
+          peer.call(c.peer, stream); 
+        });
+      },
+      connectSendPeerId : function(id) {
+        console.log("[webRTC] ServerId: Connecting to "+id+" send our ID...");
+        var conn = peer.connect(id, {
+            reliable: true
+        });
+        conn.on('open', function () {
+          console.log("[WebRTC] ServerId: Connected - send our ID" );
+          conn.send( {t:"serverId",id:options.peerId});
+          setTimeout(function() {
+            console.log("[WebRTC] ServerId: Closing");
+            conn.close();
+          }, 1000);
+        });
       }
     });
   } else {
     Object.assign(options, { // ============== CLIENT
       getPorts : function(cb) { // CLIENT
         callbackAddWithTimeout("getPorts", cb, 2000);
-        conn.send({t:"getPorts"});      
+        options.connection.send({t:"getPorts"});      
       },
       portConnect : function(port, cb) { // CLIENT
         callbackAddWithTimeout("connect", cb, 2000);
-        conn.send({t:"connect", port:port});      
+        options.connection.send({t:"connect", port:port});      
       },
       portDisconnect : function(cb) { // CLIENT
         callbackAddWithTimeout("disconnect", cb, 2000);
-        conn.send({t:"disconnect"});      
+        options.connection.send({t:"disconnect"});      
       },
       portWrite : function(data, cb) { // CLIENT
         callbackAddWithTimeout("write", cb, 2000);
-        conn.send({t:"write", data:data});      
+        options.connection.send({t:"write", data:data});      
       },
     });
   }
+
+  // ---------------------------------
+
+  function callbackAddWithTimeout(id, cb, timeout) {
+    callbacks[id] = cb;
+    // TODO: timeout?
+  }
+  function callbackCall(id, data) {
+    if (callbacks[id])
+      callbacks[id](data);
+    callbacks[id] = undefined;
+    // TODO: timeout?
+  }
+  
+  
+  
+  function webrtcAddHandlers(conn) {
+    options.connection = conn; 
+    options.connections.push(conn);
+    conn.on('open', function () {
+      console.log("[WebRTC] Connected to: " + conn.peer);
+        //conn.send(command);
+      /*if (options.bridge)
+        {t:"serverId",id:"..."}*/
+    });
+    // Handle incoming data (messages only since this is the signal sender)
+    conn.on('data', function (data) {
+      webrtcDataHandler(conn, data, options);      
+    });
+    conn.on('close', function () {
+      console.log("Connection closed");
+      options.connection = undefined;
+      var idx = options.connections.indexOf(conn);
+      if (idx>=0)
+        options.connections.splice(idx,1);
+    });  
+  }
+  
+  function webrtcDataHandler(conn, data, options) {
+    console.log("[webrtc] data " + JSON.stringify(data));
+    if ("object" != typeof data) return;
+    if (options.bridge) switch (data.t) { // ================= BRIDGE
+      case "getPorts" : // BRIDGE
+        options.onGetPorts(ports => {        
+          conn.send({t:"getPortsResponse", ports:ports});
+        });
+        break;
+      case "connect" : // BRIDGE
+        options.onPortConnect(data.port, () => {        
+          conn.send({t:"connectResponse"});
+        });
+        break;
+      case "disconnect" : // BRIDGE
+        options.onPortDisconnect(data.port, () => {        
+          conn.send({t:"disconnectResponse"});
+        });
+        break;
+      case "write" : // BRIDGE
+        options.onPortWrite(data.data, () => {        
+          conn.send({t:"writeResponse"});
+        });
+        break;
+    } else switch (data.t) { // ======================CLIENT
+      case "getPortsResponse" : // CLIENT
+        data.ports.forEach(port => {
+          // TODO set port.type so we get a different icon?
+          if (port.description)
+            port.description = "REMOTE: "+port.description;
+        });
+        callbackCall("getPorts", data.ports);
+        break;  
+      case "connectResponse" : // CLIENT
+        callbackCall("connect");
+        break;  
+      case "disconnected" : // CLIENT
+        options.onPortDisconnected();
+        break;       
+      case "disconnectResponse" : // CLIENT
+        callbackCall("disconnect");
+        break;     
+      case "writeResponse" : // CLIENT
+        callbackCall("write");
+        break;     
+      case "received" : // CLIENT
+        options.onPortReceived(data.data);
+        break;        
+    }  
+  }
+
+  // -----------------------------------------------------
   
   return options;
 }
 
 
-function callbackAddWithTimeout(id, cb, timeout) {
-  callbacks[id] = cb;
-  // TODO: timeout?
-}
-function callbackCall(id, data) {
-  if (callbacks[id])
-    callbacks[id](data);
-  callbacks[id] = undefined;
-  // TODO: timeout?
-}
-
-/* Create a connection:
-options = {
-  peerId
-} */
-function webrtcConnect(options) {
-  options = options||{};
-  if (options.connectToPeerID===null) {
-    console.log("No Peer ID specified!");
-    console.log("Please launch this page from the QR Code or Link in the Web IDE");
-    return;
-  }
-  if (conn) conn.close(); // close old
-  console.log("Connecting...");
-  conn = peer.connect(options.connectToPeerID, {
-      //reliable: true
-  });
-  webrtcAddHandlers(conn, options);
-}
-
-function webrtcAddHandlers(conn, options) {
-  conn.on('open', function () {
-    console.log("Connected to: " + conn.peer);
-      //conn.send(command);
-  });
-  // Handle incoming data (messages only since this is the signal sender)
-  conn.on('data', function (data) {
-    webrtcDataHandler(conn, data, options);      
-  });
-  conn.on('close', function () {
-    console.log("Connection closed");
-  });  
-}
-
-
-function webrtcDataHandler(conn, data, options) {
-  console.log("[webrtc] data " + JSON.stringify(data));
-  if ("object" != typeof data) return;
-  if (options.bridge) switch (data.t) { // ================= BRIDGE
-    case "getPorts" : // BRIDGE
-      options.onGetPorts(ports => {        
-        conn.send({t:"getPortsResponse", ports:ports});
-      });
-      break;
-    case "connect" : // BRIDGE
-      options.onPortConnect(data.port, () => {        
-        conn.send({t:"connectResponse"});
-      });
-      break;
-    case "disconnect" : // BRIDGE
-      options.onPortDisconnect(data.port, () => {        
-        conn.send({t:"disconnectResponse"});
-      });
-      break;
-    case "write" : // BRIDGE
-      options.onPortWrite(data.data, () => {        
-        conn.send({t:"writeResponse"});
-      });
-      break;
-  } else switch (data.t) { // ======================CLIENT
-    case "getPortsResponse" : // CLIENT
-      data.ports.forEach(port => {
-        // TODO set port.type so we get a different icon?
-        if (port.description)
-          port.description = "REMOTE: "+port.description;
-      });
-      callbackCall("getPorts", data.ports);
-      break;  
-    case "connectResponse" : // CLIENT
-      callbackCall("connect");
-      break;  
-    case "disconnected" : // CLIENT
-      options.onPortDisconnected();
-      break;       
-    case "disconnectResponse" : // CLIENT
-      callbackCall("disconnect");
-      break;     
-    case "writeResponse" : // CLIENT
-      callbackCall("write");
-      break;     
-    case "received" : // CLIENT
-      options.onPortReceived(data.data);
-      break;        
-  }  
-}
